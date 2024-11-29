8a56e82fb29c0b2e2049215c57676111e12be89c
Letícia <leticiafanan@gmail.com>
2024-11-29 17:21:58 -0300

    first commit

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+const globalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    cursor: pointer;
+  }
+
+`;
+
+export default globalStyles;
